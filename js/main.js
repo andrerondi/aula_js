e23615b70252e15257606040b3b328bfe0327c1d
andrerondi <andre_castro_rondi@hotmail.com>
2019-11-24 18:53:22 -0400

    Introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "André Rondi";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos");
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
